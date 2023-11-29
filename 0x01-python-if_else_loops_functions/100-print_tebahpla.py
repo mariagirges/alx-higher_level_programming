adad180b5a114cc6ebe32c2e49a4bc24bd7b80c2
mariagirges <mariagirges07@gmail.com>
2023-11-29 12:13:36 -0800

    14. Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for char in range(ord('z'), ord('a') - 1, -1):
     case = "lower" if char % 2 == 0 else "upper"
-    print("{}".format(chr(char).upper() if case == "upper" else chr(char)), end="")
+    print("{}".format(chr(char).upper() if case == "upper"else chr(char)), end="")
