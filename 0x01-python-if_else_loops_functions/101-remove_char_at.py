1173ddd0ef18488a2ad9c4460c49456ec437af66
mariagirges <mariagirges07@gmail.com>
2023-11-29 12:48:21 -0800

    101-remove_char_at.py

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,7 @@
+def remove_char_at(str, n):
+    # Check if n is a valid index
+    if 0 <= n < len(str):
+        result = str[:n] + str[n + 1:]
+        return result
+    else:
+        returnstr
