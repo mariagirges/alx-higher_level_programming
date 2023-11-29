0f68ed63d9e3ad9b9b6dc9b99cebda0b9e064cbf
mariagirges <mariagirges07@gmail.com>
2023-11-29 09:13:00 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-for x in range(1,10):
+for x in range(0,10):
     for y in range(x+1,10):
-        if x == 8:
-            if y == 9:
-                print (89)
-        print("{:d}{:d}".format(x, y), end=", ")
+        print("{:d}{:d}".format(x, y), end=", "if x < 8 else "\n")
