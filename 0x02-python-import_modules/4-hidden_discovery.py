07f036a8e8088ed869cf4df64a68496144fb704d
mariagirges <mariagirges07@gmail.com>
2023-11-30 14:32:06 -0800

    python

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     Name = dir(hidden_4)
     for name in Name:
-    if name[:2] != "__":
-        print(name)
+        if name[:2] != "__":
+            print(name)
