248f383b9f3e70422f071b2482c7fc48ef9ec7cd
mariagirges <mariagirges07@gmail.com>
2023-11-30 13:53:21 -0800

    3. Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    summation = 0
+    for x in range(len(sys.argv) - 1):
+        summation += int(sys.argv[x + 1])
+    print("{}".format(total))
